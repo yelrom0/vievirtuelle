53bbb430687c7afdb5b9026ee6ff260b7065543a
yelrom0 <yelrom0@gmail.com>
2024-08-14 16:57:15 +1000

    chore(Main Menu): Add top docstring and class_name

--- assets/ui/main_menu/scripts/main_menu.gd
+++ assets/ui/main_menu/scripts/main_menu.gd
@@ -1,4 +1,14 @@
-extends TabContainer
+"""
+main_menu.gd
+
+This script is attached to the main
+menu node in the main scene.
+
+For now all it does is add a debug tab
+but eventually it will be used to control
+other aspects of the main scene.
+"""
+class_name MainMenu extends TabContainer
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
